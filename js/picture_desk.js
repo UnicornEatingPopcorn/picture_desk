ea1e985bd25dd9a0a670c4ee850fb69e38ab741f
anna_evlanova <malory6072@gmail.com>
2018-11-17 18:35:38 +0300

    fix errors wirh id

--- js/picture_desk.js
+++ js/picture_desk.js
@@ -26,12 +26,14 @@ class Picture {
     const pictureDesk = document.querySelector('.images');
     const newPicture = document.createElement('div');
     newPicture.setAttribute('class', 'image-container');
+    newPicture.setAttribute('data-id', this.id);
+
 
     newPicture.innerHTML = `
-    <h2><button class="btn btn-info editButton"><i class="fas fa-edit"></i></button><button class="btn btn-success okButton">OK</button> ${this.title}</h2>
+    <h2><button class="btn btn-info editButton"><i class="fas fa-edit"></i></button><button class="btn btn-success okButton">OK</button><span class="pictureTitle"> ${this.title}</span></h2>
     <input type="text" class="form-control editTitle">
     <div class="image-wrapper"><img src="${this.src}" class="img-thumbnail" alt="Responsive image">
-    <button data-id="${this.id}" class="btn btn-danger deleteButton" type="delete">X</button></div>`;
+    <button class="btn btn-danger deleteButton" type="delete">X</button></div>`;
 
     pictureDesk.appendChild(newPicture);
   }
@@ -42,13 +44,22 @@ class Picture {
 
     // Передает правильный заголовок в запросе
     xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onreadystatechange = function handleResponse() { // Вызывает функцию при смене состояния.
+
+    const picture = this;
+    xhr.onreadystatechange = function handleResponse(event) {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        const updatedPicture = JSON.parse(xhr.response);
+        const imageContainer = document.querySelector(`[data-id="${picture.id}"]`);
+        const okButton = imageContainer.querySelector('.okButton');
+
+        okButton.style.display = 'none';
+        imageContainer.querySelector('.editButton').style.display = 'block';
+        imageContainer.querySelector('.editTitle').style.display = 'none';
+        imageContainer.querySelector('.pictureTitle').innerText = picture.title;
       }
     };
 
-    xhr.send(JSON.stringify({ picture: { src: this.src, id: this.id, title: this.title } }));
+
+    xhr.send(JSON.stringify({ picture: { id: this.id, title: this.title } }));
   }
 
   static index() {
@@ -81,7 +92,7 @@ class Picture {
     const picture = this;
     xhr.onreadystatechange = function handleResponse() { // Вызывает функцию при смене состояния
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 204) {
-        document.querySelector(`[data-id="${picture.id}"]`).parentElement.parentElement.remove();
+        document.querySelector(`[data-id="${picture.id}"]`).remove();
       }
     };
 
@@ -101,14 +112,14 @@ addButton.onclick = function createPictureByClick(event) {
 document.querySelector('.images').addEventListener('click', (event) => {
   event.preventDefault();
   if (event.target.classList.contains('deleteButton')) {
-    const { id } = event.target.dataset;
+    const { id } = event.target.closest('.image-container').dataset;
     const picture = new Picture(null, null, id);
     picture.delete();
   }
 
   if (event.target.classList.contains('editButton')) {
     const editTitle = event.target;
-    const imageContainer = editTitle.parentElement.parentElement;
+    const imageContainer = editTitle.closest('.image-container');
 
     editTitle.style.display = 'none';
     imageContainer.querySelector('.okButton').style.display = 'block';
@@ -117,21 +128,12 @@ document.querySelector('.images').addEventListener('click', (event) => {
 
   if (event.target.classList.contains('okButton')) {
     const okButton = event.target;
-    const imageContainer = okButton.parentElement.parentElement;
-    const { id } = event.target.dataset;
-    const picture = new Picture(null, null, id);
-
-    // const ok = imageContainer.querySelector('.okButton').innerText;
+    const imageContainer = okButton.closest('.image-container');
+    const { id } = event.target.closest('.image-container').dataset;
     const title = imageContainer.querySelector('.editTitle').value;
+    const picture = new Picture(null, title, id);
 
-    const newTitle = { id, title };
-
-    okButton.style.display = 'none';
-    imageContainer.querySelector('.editButton').style.display = 'block';
-    imageContainer.querySelector('.editTitle').style.display = 'none';
-    imageContainer.querySelector('.editTitle').innerText = title;
-
-    picture.update(newTitle);
+    picture.update();
   }
 });
 // добавлять картинки в контейнер с классом с максимальной шириной и высотой
